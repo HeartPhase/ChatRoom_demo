e0aec2fccbe2e55fb43c033fe500cd93768e13cd
Tommy <870667475@qq.com>
2021-06-24 01:18:01 +0800

    update

--- server/server/ForClient.cs
+++ server/server/ForClient.cs
@@ -14,12 +14,18 @@ namespace myServer
         private byte[] data = new byte[1024];//这个是一个数据容器
         private Socket socketclient;
         private Thread tt;
+        private String name;
+        private bool registed;
+        private byte op0 = 0;//握手
+        private byte op1 = 1;//发送信息
+        private byte op2 = 2;//系统消息，无用户名
+        private byte op6 = 6;//服务端报错码
 
         public ForClient(Socket s)
         {
+            registed = false;
             socketclient = s;
             //启动一个线程，处理客户端数据接收
-
             tt = new Thread(ReceiveMessage);
             tt.Start();
         }
@@ -32,6 +38,7 @@ namespace myServer
                 //在接收数据之前  判断一下socket连接是否断开
                 if (socketclient.Poll(10, SelectMode.SelectRead))
                 {
+                    Console.WriteLine("tcp 客户端已断开");
                     socketclient.Close();
                     break;//跳出循环 终止线程的执行
                 }
@@ -41,12 +48,33 @@ namespace myServer
                     try
                     {
                         int length = socketclient.Receive(data);
-                        string message = Encoding.UTF8.GetString(data, 0, length);
+                        if (data[1]==op0) {//用户名申请
+                            String userName =Encoding.UTF8.GetString(data, 2, length);
+                            if (!Program.nameIsExit(userName)) {
+                                byte[] temp = new byte[] {0,0};
+                                SendMessage(temp);
+                                this.name = userName;
+                                registed = true;
+                                Console.WriteLine("客户端：" + socketclient.RemoteEndPoint +"  注册用户名成功："+name);
+                            }
+                            
+                        }
 
-                        //接收到数据的时候 要把这个数据 分发到客户端
+                        if (data[1]==op1) {//接受聊天数据
+                            if (!registed) {//未注册不得聊天
+                                Console.WriteLine("客户端未注册用户名，不得聊天");
+                                continue;
+                            }
+                            int i = 2;
+                            while (data[i]!=op0) {
+                                i++;
+                            }
+                            String message = Encoding.UTF8.GetString(data,i+1,length);
+                            Console.WriteLine("收到了客户端：" + socketclient.RemoteEndPoint + "  " + "消息: " + message);
+                        }
                         //广播这个消息
-                        Program.BroadcastMessage(socketclient.RemoteEndPoint + message);
-                        Console.WriteLine("收到了客户端：" + socketclient.RemoteEndPoint + "消息:" + message);
+                        Program.BroadcastMessage(data);
+                        data = new byte[1024];
                     }
 
                     catch
@@ -57,12 +85,23 @@ namespace myServer
             }
         }
 
-        public void SendMessage(string message)
-        {
 
-            byte[] data = Encoding.UTF8.GetBytes(message);
-            socketclient.Send(data);
+        
+        public bool nameIsMacth(String userName) {
+            if (userName == name)
+                return true;
+            return false;
         }
+
+        public void SendMessage(byte[] data)
+        {
+            try {
+                socketclient.Send(data);
+            }
+            catch (Exception ex) { }
+            
+        }
+
         public bool Connected
         {
             get { return socketclient.Connected; }
--- server/server/Program.cs
+++ server/server/Program.cs
@@ -13,17 +13,20 @@ namespace myServer
 
             Socket TcpServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             String ip = GetLocalIPv4();
+            //String ip = "127.0.0.1";
+            int port = 7788;
             if (ip!=null) {
-                Console.WriteLine("服务器开启，本机IP： "+ip+"\n"+"请告知客户端。");
+                TcpServer.Bind(new IPEndPoint(IPAddress.Parse(ip), port));
+                Console.WriteLine("服务器开启，本机IP： " + ip + "  端口号：" + port + "\n" + "请告知客户端。" + "\n");
+                TcpServer.Listen(100);
             }
-            TcpServer.Bind(new IPEndPoint(IPAddress.Parse(ip), 7788));
-            TcpServer.Listen(100);
+            
             
 
             while (true)
             {
                 Socket clientSocket = TcpServer.Accept();
-                Console.WriteLine("新接入一个客户端");
+                Console.WriteLine("新接入一个客户端，IP地址："+ clientSocket.RemoteEndPoint);
                 ForClient cc = new ForClient(clientSocket);//每个客户端通信逻辑放到client
                 clientlist.Add(cc);
             }
@@ -43,16 +46,26 @@ namespace myServer
             return null;
         }
 
+        public static bool nameIsExit(String userName) {
+            bool exit = false;
+            foreach (var clients in clientlist)
+            {
+                if (clients.nameIsMacth(userName))
+                    exit = true;
+
+            }
+            return exit;
+        }
 
         //广播信息，同时清除断连客户
-        public static void BroadcastMessage(string message)
+        public static void BroadcastMessage(byte[] pck)
         {
             var notConnectedList = new List<ForClient>();
 
             foreach (var clients in clientlist)
             {
                 if (clients.Connected)
-                    clients.SendMessage(message);
+                    clients.SendMessage(pck);
                 else
                 {
                     notConnectedList.Add(clients);
